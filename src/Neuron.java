ba54634fe056ea612cd135918c5ec13cab50822f
fathanpranaya <fathan.pranaya@gmail.com>
2014-10-18 10:45:18 +0700

    balikan

--- src/Neuron.java
+++ src/Neuron.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.List;
 
 import weka.core.Instance;
@@ -6,22 +5,20 @@ import weka.core.Instances;
 
 public class Neuron {
 	private Integer activationFunction = 1; // 1. sigmoid, 2. step, 3. sign
-	private ArrayList<Double> inputWeight;
-	private ArrayList<ArrayList<Double>> input;
+	private double[] inputWeight;
+	private double[][] input;
 	private int idx_instance;
 	private double threshold; // buat threshold step
 
 	public Neuron(Instances data) {
 		this.threshold = 0.5;
 		idx_instance = 0;
-		input = new ArrayList<ArrayList<Double>>();
-		inputWeight = new ArrayList<Double>();
-		
+		input = new double[100][100];
 		for (int i = 0; i < data.numInstances(); i++) {
 			for (int j = 0; j < data.get(i).numAttributes()-1; j++) {
-				input.set(i, data.get(i).value(j));
-				System.out.print(data.get(i).value(j));
-				//System.out.println(input[i][j]+" -- ");
+				input[i][j] = data.get(i).value(j);
+				//System.out.print(data.get(i).value(j));
+				System.out.print(input[i][j]+" -- ");
 			}
 			System.out.println();
 		}
@@ -29,8 +26,8 @@ public class Neuron {
 
 	public double summingFunction() {
 		double sum = 0;
-		for (int i = 0; i < inputWeight.size(); i++) {
-			sum += inputWeight.get(i) * input.get(idx_instance).get(i);
+		for (int i = 0; i < inputWeight.length; i++) {
+			sum += inputWeight[i] * input[i][idx_instance];
 		}
 		return sum;
 	}
